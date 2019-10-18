ddd66be3f0520ffbb681e3e9704e970acbc1da7e
Jacqueline Remmel <jpremmel@gmail.com>
2019-10-18 08:53:48 -0700

    Update backend models (stylist and client classes).

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+    public class Stylist
+    {
+        public Stylist()
+        {
+            this.Clients = new HashSet<Client>();
+        }
+        public virtual ICollection<Client> Clients { get; set; }
+        public int StylistId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
