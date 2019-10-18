a0f8ccf58974f4edfb1094faa3c6fb25010aba83
Jacqueline Remmel <jpremmel@gmail.com>
2019-10-18 11:16:25 -0700

    Add ability to add clients from a particular stylist's details page.

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -16,6 +16,18 @@ namespace HairSalon.Controllers
             _db = db;
         }
 
-        //Route methods go here
+        public ActionResult Create(int id)
+        {
+            ViewBag.StylistId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Client client)
+        {
+            _db.Clients.Add(client);
+            _db.SaveChanges();
+            return RedirectToAction("Details", "Stylists", new { id = client.StylistId });
+        }
     }
 }
